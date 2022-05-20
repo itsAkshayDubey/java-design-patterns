1ca52af92a2a35151fe28762816d5feea1a8e8b1
Akshay Dubey <akshay.dubey055@gmail.com>
2022-05-20 14:13:23 +0530

    refactor: Add sysout statements in strategy pattern demo class

--- src/main/java/com/akshay/design/patterns/behavioral/stratergy/StratergyDemo.java
+++ src/main/java/com/akshay/design/patterns/behavioral/stratergy/StratergyDemo.java
@@ -5,10 +5,13 @@ public class StratergyDemo {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Executing Addition Strategy");
 		System.out.println(new Context(new Addition()).executeStratergy(1, 2));
 		
+		System.out.println("Executing Subtraction Strategy");
 		System.out.println(new Context(new Subtraction()).executeStratergy(5, 1));
 		
+		System.out.println("Executing Multiplication Strategy");
 		System.out.println(new Context(new Multiplication()).executeStratergy(5, 3));
 	}
 
