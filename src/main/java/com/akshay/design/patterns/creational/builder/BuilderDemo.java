72ab760b6e2beced921008234e6c39f555507ecf
Akshay Dubey <akshay.dubey055@gmail.com>
2022-06-04 14:13:23 +0530

    refactor: Add sysout statement to builder design pattern

--- src/main/java/com/akshay/design/patterns/creational/builder/BuilderDemo.java
+++ src/main/java/com/akshay/design/patterns/creational/builder/BuilderDemo.java
@@ -6,7 +6,7 @@ public class BuilderDemo {
 
 	public static void main(String[] args) {
 		
-		
+		System.out.println("Building phone object using known attributes...");
 		Phone p = new PhoneBuilder().setOs("Android").setBattery(3000).getPhone();
 		System.out.println(p);
 
