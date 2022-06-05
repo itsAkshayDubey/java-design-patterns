ccc50439d78be73d1bb3d02cf85c3a4ad38c1c78
Akshay Dubey <akshay.dubey055@gmail.com>
2022-06-06 01:13:42 +0530

    Add bike class to bridge pattern

---
+++ src/main/java/com/akshay/design/patterns/structural/bridge/Bike.java
@@ -0,0 +1,16 @@
+package com.akshay.design.patterns.structural.bridge;
+
+class Bike extends Vehicle {
+    public Bike(Workshop workShop1, Workshop workShop2)
+    {
+        super(workShop1, workShop2);
+    }
+ 
+    @Override
+    public void manufacture()
+    {
+        System.out.print("Bike ");
+        workShop1.work();
+        workShop2.work();
+    }
+}
